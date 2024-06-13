50569151c7a5020ff9d11c8e78f518deecd496a6
JoyChudasama <joychudasama11@gmail.com>
2024-06-12 21:35:55 -0300

    added monaco-editor and removed unnecessary files and code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import React, { useRef } from "react";
+
+import Editor from "@monaco-editor/react";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const editorRef = useRef(null);
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
+  }
+
+  function showValue() {
+    const currentCode = editorRef.current?.getValue()
+    
+    // TODO: Make api call and show output
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={showValue}>Run</button>
+      <Editor
+        height="100vh"
+        defaultLanguage="javascript"
+        defaultValue="//Write your code here"
+        theme="vs-dark"
+        onMount={handleEditorDidMount}
+      />
     </>
-  )
+  );
 }
 
 export default App
